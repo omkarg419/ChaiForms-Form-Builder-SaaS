05c417999684bc6729e268bbc278a060c399aeb2
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-05-27 01:51:26 +0530

    feat: add hooks for creating forms and fetching user forms

---
+++ apps/web/hooks/api/form/index.ts
@@ -0,0 +1,56 @@
+import { trpc } from "~/trpc/client";
+
+export const useCreateForm = () => {
+  const utils = trpc.useUtils();
+  const {
+    mutateAsync: createFormAsync,
+    mutate: createForm,
+    error,
+    isError,
+    isIdle,
+    isSuccess,
+    reset,
+    status,
+  } = trpc.form.createForm.useMutation({
+    onSuccess: async () => {
+      await utils.form.getFormsByUser.invalidate();
+    },
+  });
+
+  return {
+    createFormAsync,
+    createForm,
+    error,
+    isError,
+    isIdle,
+    isSuccess,
+    reset,
+    status,
+  };
+};
+
+export const useGetFormsByUser = () => {
+  const {
+    data: forms,
+    error,
+    isError,
+    isSuccess,
+    refetch,
+    status,
+    isLoading,
+    isFetching,
+    isFetched,
+  } = trpc.form.getFormsByUser.useQuery();
+
+  return {
+    forms,
+    error,
+    isError,
+    isSuccess,
+    refetch,
+    status,
+    isLoading,
+    isFetching,
+    isFetched,
+  };
+};
