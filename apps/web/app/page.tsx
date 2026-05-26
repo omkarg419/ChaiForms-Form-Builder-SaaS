305b64bfb072c1cbd209dfabf62d3c54d94b73c3
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-05-26 23:00:36 +0530

    chore: update dependencies and upgrade recharts version
    - Added new dependencies: @dnd-kit/core, @dnd-kit/modifiers, @dnd-kit/sortable, @dnd-kit/utilities, @tabler/icons-react, and @tanstack/react-table.
    - Upgraded recharts from version 2.15.4 to 3.8.0.
    - Updated pnpm-lock.yaml to reflect changes in package versions and dependencies.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,30 @@
-import { api } from "~/trpc/server";
+"use client";
 
-export default async function Home() {
-  const { status } = await api.health.getHealth.query();
+import { useEffect } from "react";
+import{useRouter} from "next/navigation";
+import { useGetLoggedInUser } from "~/hooks/api/auth";
+
+export default  function Home() {
   
+  const { user } = useGetLoggedInUser();
+const router = useRouter();
+console.log("User from useGetLoggedInUser hook:", user);
+  useEffect(() => {
+
+
+    if(user && user.id){
+      router.replace("/dashboard");
+    }
+    else{
+      router.replace("/signin");
+    }
+  },[user,router]);
+
   return (
     <main className="min-h-screen min-w-screen flex justify-center items-center">
       <div>
         <h1 className="text-3xl">ChaiForms</h1>
-        <h2>Server Status: {status}</h2>
+        <h2>{JSON.stringify(user,null,2)}</h2>
 
       </div>
     </main>
