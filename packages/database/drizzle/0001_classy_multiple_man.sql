1a9fb4f768cc45517e2339211d7a72868399d3f3
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-05-26 23:47:05 +0530

    feat: add forms table and model, update schema exports, and modify journal entries

---
+++ packages/database/drizzle/0001_classy_multiple_man.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "forms" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(55) NOT NULL,
+	"description" varchar(300),
+	"created_by" uuid NOT NULL,
+	"is_published" boolean DEFAULT false,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "forms" ADD CONSTRAINT "forms_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
